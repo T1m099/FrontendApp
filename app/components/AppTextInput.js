374cd2b2ded1f69c9b7d0f207f695d72dcf47f93
= <=>
2021-02-24 16:08:57 +0100

    Added a basic text component for a homogenous look across the app
    Added some form components from an older project
    
    Updated default components with new naming scheme
    
    Refactoring on the default components

---
+++ app/components/AppTextInput.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import defaultStyles from '../config/styles';
+import colors from '../config/colors';
+
+function AppTextInput({ icon, width = '100%', ...otherProps }) {
+	return (
+		<View style={[styles.container, { width }]}>
+			{icon && (
+				<MaterialCommunityIcons
+					name={icon}
+					size={20}
+					color={defaultStyles.colors.medium}
+					style={styles.icon}
+				/>
+			)}
+			<TextInput
+				placeholderTextColor={colors.text}
+				style={defaultStyles.text}
+				{...otherProps}
+			/>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: colors.primary,
+		borderRadius: 25,
+		flexDirection: 'row',
+		padding: 15,
+		marginVertical: 10,
+	},
+	icon: {
+		marginRight: 10,
+	},
+});
+
+export default AppTextInput;
---
+++ app/components/DatePickerInput.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+import AppText from './AppText';
+import colors from '../config/colors';
+
+function DatePickerInput({
+	value,
+	valueToDisplay,
+	mode = 'date',
+	onDateSelection,
+	style,
+}) {
+	const [visible, setVisible] = useState(false);
+	return (
+		<TouchableOpacity
+			onPress={() => {
+				setVisible(true);
+			}}
+			style={style}
+		>
+			<View style={[styles.container, style]}>
+				<AppText style={styles.text}>{valueToDisplay}</AppText>
+				{visible && (
+					<DateTimePicker
+						testID='dateTimePicker'
+						value={value}
+						mode={mode}
+						is24Hour={true}
+						display='spinner'
+						onChange={(event, selectedDate) => {
+							setVisible(false);
+							onDateSelection(event, selectedDate);
+						}}
+					/>
+				)}
+			</View>
+		</TouchableOpacity>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		padding: 15,
+		flexDirection: 'row',
+		backgroundColor: colors.primary,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 25,
+		width: '100%',
+	},
+	text: {
+		fontWeight: 'bold',
+	},
+});
+
+export default DatePickerInput;
