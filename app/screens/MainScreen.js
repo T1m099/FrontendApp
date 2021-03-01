560f5d95892ea0fb51c9f81e06c8564bfdb8f392
MarcDKu <marc-kustermann@gmx.de>
2021-03-01 17:16:27 +0100

    Add Background Image and add Additional Styles

--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
-
+import {View, Text, Button,StyleSheet, ImageBackground} from 'react-native';
 import SafeAreaScreen from '../components/SafeAreaScreen';
 import routes from '../navigation/routes';
 
+
 function MainScreen({ navigation }) {
+
 	return (
 		<SafeAreaScreen>
+			<ImageBackground source={require('../images/Background.png')} style={styles.image}>
+
 			<View style={styles.container}>
 				<Text>Homescreen</Text>
 				<Button
-					title='Go to calendar'
+					borderRadius:100
 					onPress={() => {
 						navigation.navigate(routes.ORGANIZATION, {
 							screen: routes.CALENDAR,
@@ -18,12 +21,38 @@ function MainScreen({ navigation }) {
 					}}
 				></Button>
 			</View>
+				<View style={styles.container}>
+					<Text>Homescreen</Text>
+					<Button style={styles.button}
+						title='Go to calendar'
+						onPress={() => {
+							navigation.navigate(routes.ORGANIZATION, {
+								screen: routes.CALENDAR,
+							});
+						}}
+					></Button>
+				</View>
+
+			</ImageBackground>
+
 		</SafeAreaScreen>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: { justifyContent: 'center', alignItems: 'center', flex: 1 },
+	container: { flex:1, flexDirection:'row', alignSelf:'center',  width: '92%', maxHeight:'10%', borderRadius:10 ,marginTop:'.75%', marginBottom: '.75%', backgroundColor: 'rgba(0,0,0,.5)'},
+	image: {
+		flex: 1,
+		resizeMode: "cover",
+		justifyContent: "center"
+	},
+	button:{    width: 100,
+		height: 100,
+		justifyContent: 'center',
+		alignItems: 'center',
+		padding: 10,
+		borderRadius: 100,
+		backgroundColor: 'orange',  }
 });
 
 export default MainScreen;
