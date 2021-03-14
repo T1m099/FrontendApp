0d79e1f75026c33e6041c1c93d957f8c4af7b102
= <=>
2021-03-14 17:07:23 +0100

    Added option to track mood with symptom

--- app/config/eventTypes.js
+++ app/config/eventTypes.js
@@ -19,17 +19,47 @@ export const eventTypes = {
 	},
 	Disease: {
 		end: new Date(),
-		disease: '',
+		symptom: '',
 	},
 	Symptom: {
-		disease: '',
+		symptom: '',
+		mood: '',
 	},
 };
 
 export const allAdditionalProperties = {
 	end: new Date(),
 	reminders: [],
-	disease: '',
+	symptom: '',
+	mood: '',
 };
 
 export const typeNames = Object.keys(eventTypes);
+
+export const markingColors = [
+	'#00ff00',
+	'#0f0f00',
+	'#006CBE',
+	'#427505',
+	'#C33400',
+	'#00345C',
+	'#E3CC00',
+	'#00A3AE',
+	'#CC007E',
+	'#684697',
+	'#B10E1C',
+	'#F9A825',
+];
+
+export const symptoms = [
+	'Husten',
+	'Zäher Schleim',
+	'Atemnot',
+	'Infekte',
+	'Pneumonie',
+	'Bauchschmerzen',
+	'Verstopfung',
+	'Fettige Stühle',
+];
+
+export const moods = ['Great', 'Good', 'Okay', 'Meh', 'Bad', 'Terrible'];
