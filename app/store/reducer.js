672149a39053e697004191c49ecb0bb1a53dc0a9
= <=>
2021-03-23 18:57:34 +0100

    Added settings to redux store

--- app/store/reducer.js
+++ app/store/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import entitiesReducer from './entities';
 import authReducer from './auth';
+import settingReducer from './settings';
 
 export default combineReducers({
 	entities: entitiesReducer,
 	auth: authReducer,
+	settings: settingReducer,
 });
---
+++ app/store/settings.js
@@ -0,0 +1,38 @@
+import { createSlice, action } from '@reduxjs/toolkit';
+import { createSelector } from 'reselect';
+
+const baseSettings = {
+	trackingCategories: [{ category: 'Blutzucker', unit: 'mg' }],
+};
+
+const slice = createSlice({
+	name: 'settings',
+	initialState: baseSettings,
+	reducers: {
+		trackingCategoryAdded: (settings, action) => {
+			settings.trackingCategories.push(action.payload);
+		},
+		trackingCategoryRemoved: (settings, action) => {
+			settings.trackingCategories = settings.trackingCategories.filter(
+				c => c.category !== action.payload.category
+			);
+		},
+		settingsReset: (settings, action) => {
+			settings.credentials = { ...baseSettings };
+		},
+	},
+});
+
+const { trackingCategoryAdded, settingsReset } = slice.actions;
+export default slice.reducer;
+
+export const addTrackingCategory = category => async dispatch => {
+	dispatch(trackingCategoryAdded(category));
+};
+
+//Selectors
+export const getTrackingCategories = () =>
+	createSelector(
+		state => state.settings,
+		settings => settings.trackingCategories
+	);
