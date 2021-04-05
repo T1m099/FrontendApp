06fc3de04151bfa2d9ea9d1e522f7dd86e53301b
= <=>
2021-04-05 15:13:28 +0200

    removed tracking categories from settings

--- app/screens/SettingsScreen.js
+++ app/screens/SettingsScreen.js
@@ -1,76 +1,67 @@
 import React from 'react';
-import {View, StyleSheet, ImageBackground, Text} from 'react-native';
-import {useDispatch} from 'react-redux';
-import {logout} from '../store/auth';
-import {AntDesign} from "@expo/vector-icons";
-import {ButtonDecline, ButtonMagenta} from "../components/Buttons";
+import { View, StyleSheet, ImageBackground, Text } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { logout } from '../store/auth';
+import { AntDesign } from '@expo/vector-icons';
+import { ButtonDecline, ButtonMagenta } from '../components/Buttons';
 import routes from '../navigation/routes';
-import colors from "../config/colors";
+import colors from '../config/colors';
 
-function SettingsScreen({navigation}) {
-    const dispatch = useDispatch();
+function SettingsScreen({ navigation }) {
+	const dispatch = useDispatch();
 
-    const handleLogout = () => {
-        dispatch(logout());
-    };
-    return (
-        <ImageBackground source={ require('../images/Background.png') } style={ styles.image }>
-            <View style={ styles.container }>
-                <Text style={ styles.text }>Logout</Text>
+	const handleLogout = () => {
+		dispatch(logout());
+	};
 
-                <ButtonDecline
-                    Content={ <AntDesign name="logout" size={ 24 } color="white"/> }
-                    onPress={ handleLogout }
-                    margin={ 8 }
-                />
-            </View>
-            <View style={ styles.container }>
-                <Text style={ styles.text }>Tracking</Text>
-                <ButtonMagenta
-                    Content={ <AntDesign name="edit" size={ 24 } color="white"/> }
-                    onPress={ () => {
-                        navigation.push(routes.TRACKING_CATEGORY_EDIT);
-                    } }
+	return (
+		<ImageBackground
+			source={require('../images/Background.png')}
+			style={styles.image}
+		>
+			<View style={styles.container}>
+				<Text style={styles.text}>Logout</Text>
 
-                    margin={ 8 }
-                />
-            </View>
-        </ImageBackground>
-
-    );
+				<ButtonDecline
+					Content={
+						<AntDesign name='logout' size={24} color='white' />
+					}
+					onPress={handleLogout}
+					margin={8}
+				/>
+			</View>
+		</ImageBackground>
+	);
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        alignSelf: 'center',
-        width: '92%',
-        maxHeight: 81,
-        borderRadius: 10,
-        marginTop: '.75%',
-        marginBottom: '.75%',
-        backgroundColor: 'rgba(0,0,0,.5)',
-        textAlign: 'center',
-        justifyContent: 'space-between'
-    },
-    image: {
-        flex: 1,
-        resizeMode: "cover",
-        justifyContent: "center"
-    },
-    text: {
-        alignItems: 'flex-start',
-        flex: 1,
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: colors.text,
-        marginLeft: '2%',
-
-    }
+	container: {
+		flex: 1,
+		flexDirection: 'row',
+		alignItems: 'center',
+		alignSelf: 'center',
+		width: '92%',
+		maxHeight: 81,
+		borderRadius: 10,
+		marginTop: '.75%',
+		marginBottom: '.75%',
+		backgroundColor: 'rgba(0,0,0,.5)',
+		textAlign: 'center',
+		justifyContent: 'space-between',
+	},
+	image: {
+		flex: 1,
+		resizeMode: 'cover',
+		justifyContent: 'center',
+	},
+	text: {
+		alignItems: 'flex-start',
+		flex: 1,
+		fontSize: 18,
+		fontWeight: 'bold',
+		color: colors.text,
+		marginLeft: '2%',
+	},
 });
-
 
 export default SettingsScreen;
