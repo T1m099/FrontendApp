694c2098316a329cb2e5c8d84bfd97635308594c
MarcDKu <marc-kustermann@gmx.de>
2021-04-18 11:51:43 +0200

    styled Disclaimer screen
    Signed-off-by: MarcDKu <marc-kustermann@gmx.de>

--- app/screens/SettingsScreen.js
+++ app/screens/SettingsScreen.js
@@ -1,115 +1,121 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, ImageBackground, Text, Modal } from 'react-native';
-import { useDispatch } from 'react-redux';
-import { logout } from '../store/auth';
-import { AntDesign } from '@expo/vector-icons';
-import { ButtonDecline, ButtonYellow } from '../components/Buttons';
+import React, {useState} from 'react';
+import {ImageBackground, Modal, StyleSheet, Text, View} from 'react-native';
+import {useDispatch} from 'react-redux';
+import {logout} from '../store/auth';
+import {AntDesign} from '@expo/vector-icons';
+import {ButtonDecline, ButtonYellow} from '../components/Buttons';
 import colors from '../config/colors';
 
 //screen with configuration options
 //Currently there is only a logout button on this screen
-function SettingsScreen({ navigation }) {
-	const dispatch = useDispatch();
-	const [securityDisclaimerVisible, setSecurityDisclaimerVisible] = useState(
-		false
-	);
+function SettingsScreen({navigation}) {
+    const dispatch = useDispatch();
+    const [securityDisclaimerVisible, setSecurityDisclaimerVisible] = useState(
+        false
+    );
 
-	const handleLogout = () => {
-		dispatch(logout());
-	};
+    const handleLogout = () => {
+        dispatch(logout());
+    };
 
-	return (
-		<ImageBackground
-			source={require('../images/Background.png')}
-			style={styles.image}
-		>
-			<View style={styles.container}>
-				<Text style={styles.text}>Logout</Text>
+    return (
+        <ImageBackground
+            source={require('../images/Background.png')}
+            style={styles.image}
+        >
+            <View style={styles.container}>
+                <Text style={styles.text}>Logout</Text>
 
-				<ButtonDecline
-					Content={
-						<AntDesign name='logout' size={24} color='white' />
-					}
-					onPress={handleLogout}
-					margin={8}
-				/>
-			</View>
-			<View style={styles.container}>
-				<Text style={styles.text}>Security Disclaimer</Text>
+                <ButtonDecline
+                    Content={
+                        <AntDesign name='logout' size={24} color='white'/>
+                    }
+                    onPress={handleLogout}
+                    margin={8}
+                />
+            </View>
+            <View style={styles.container}>
+                <Text style={styles.text}>Security Disclaimer</Text>
 
-				<ButtonYellow
-					Content={<AntDesign name='lock' size={24} color='white' />}
-					onPress={() => {
-						setSecurityDisclaimerVisible(true);
-					}}
-					margin={8}
-				/>
-			</View>
-			<Modal visible={securityDisclaimerVisible} animationType='slide'>
-				<View
-					style={[
-						styles.container,
-						{
-							maxHeight: 150,
-							flexDirection: 'column',
-							width: '100%',
-							marginTop: '100%',
-							justifyContent: 'space-evenly',
-						},
-					]}
-				>
-					<Text>
-						Your local data is fully encrypted, with the exception
-						being files, which are stored in your devices file
-						system. The files are hidden, so only this app can
-						access them.
-					</Text>
-					<Text>
-						Everything you enter in this app is sent to our servers,
-						where the data is securely stored. This is neccessary
-						for synchronization accross multiple devices.
-					</Text>
-				</View>
-				<ButtonDecline
-					Content={<AntDesign name='close' size={24} color='white' />}
-					onPress={() => {
-						setSecurityDisclaimerVisible(false);
-					}}
-					margin={8}
-				/>
-			</Modal>
-		</ImageBackground>
-	);
+                <ButtonYellow
+                    Content={<AntDesign name='lock' size={24} color='white'/>}
+                    onPress={() => {
+                        setSecurityDisclaimerVisible(true);
+                    }}
+                    margin={8}
+                />
+            </View>
+            <Modal visible={securityDisclaimerVisible} animationType='slide'>
+                <ImageBackground
+                    source={require('../images/Background.png')}
+                    style={styles.image}
+                >
+                    <View
+                        style={[
+                            styles.container,
+                            {
+                                maxHeight: 281,
+                                paddingTop: 10,
+                                flexDirection: 'column',
+                                justifyContent: 'space-evenly',
+                            },
+                        ]}
+                    >
+                        <Text style={styles.text}>
+                            Your local data is fully encrypted, with the exception
+                            being files, which are stored in your devices file
+                            system. The files are hidden, so only this app can
+                            access them.
+                        </Text>
+                        <Text style={styles.text}>
+                            Everything you enter in this app is sent to our servers,
+                            where the data is securely stored. This is neccessary
+                            for synchronization accross multiple devices.
+                        </Text>
+                        <ButtonDecline
+                            Content={'close'}
+                            onPress={() => {
+                                setSecurityDisclaimerVisible(false);
+                            }}
+                            margin={8}
+                            size={'96%'}
+                        />
+                    </View>
+
+                </ImageBackground>
+            </Modal>
+        </ImageBackground>
+    );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		flexDirection: 'row',
-		alignItems: 'center',
-		alignSelf: 'center',
-		width: '92%',
-		maxHeight: 81,
-		borderRadius: 10,
-		marginTop: '.75%',
-		marginBottom: '.75%',
-		backgroundColor: 'rgba(0,0,0,.5)',
-		textAlign: 'center',
-		justifyContent: 'space-between',
-	},
-	image: {
-		flex: 1,
-		resizeMode: 'cover',
-		justifyContent: 'center',
-	},
-	text: {
-		alignItems: 'flex-start',
-		flex: 1,
-		fontSize: 18,
-		fontWeight: 'bold',
-		color: colors.text,
-		marginLeft: '2%',
-	},
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        alignSelf: 'center',
+        width: '92%',
+        maxHeight: 81,
+        borderRadius: 10,
+        marginTop: '.75%',
+        marginBottom: '.75%',
+        backgroundColor: 'rgba(0,0,0,.5)',
+        textAlign: 'center',
+        justifyContent: 'space-between',
+    },
+    image: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
+    },
+    text: {
+        alignItems: 'flex-start',
+        flex: 1,
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: colors.text,
+        marginLeft: '2%',
+    },
 });
 
 export default SettingsScreen;
